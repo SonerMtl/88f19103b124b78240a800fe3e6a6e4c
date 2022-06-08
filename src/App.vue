31907abbb8c1d9c5ace33be40e2510b4655821b7
Soner <sonerm750@gmail.com>
2022-06-08 23:00:22 +0300

    Fetch data
    Changes on styles

--- src/App.vue
+++ src/App.vue
@@ -8,6 +8,11 @@ export default {
     return {
 
     }
+  },
+  mounted() {
+    fetch('https://teknasyon.netlify.app/.netlify/functions/products', { headers: { 'X-Access-Token' : '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' }})
+      .then(res => res.json())
+      .then(data => {console.log(data.products)})
   }
 }
 </script>
